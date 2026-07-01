f1f275b5ca7fda66a226a01541e1139a1d9ff167
FolNx <k.follower@yandex.ru>
2026-07-01 12:20:29 +0400

    update

--- test_create_orders.py
+++ test_create_orders.py
@@ -3,27 +3,24 @@
 import sender_stand_request
 import data
 
+
 # Эта функция создает заказ и возвращает track
 def create_orders_and_get_track():
-# В переменную orders_body берется тело запроса из data.ru    
+    # В переменную user_body берется тело запроса из data.py
     user_body = data.orders_body.copy()
-# В переменную response сохраняется ответ при создании нового заказа
+    # В переменную response сохраняется ответ при создании нового заказа
     response = sender_stand_request.post_new_orders(user_body)
-# В переменную track сохраняется track пришедший в ответе
+    # В переменную track сохраняется track, пришедший в ответе
     track = response.json()["track"]
     return track
 
 
-# Получаем трек и сохраняем в переменную
-track = create_orders_and_get_track()
-
-
 def test_get_order_by_track():
-# Создаём заказ и получаем его трек
+    # Создаём заказ и получаем его трек — вызов происходит внутри теста
     track = create_orders_and_get_track()
 
-# Делаем GET по треку
+    # Делаем GET по треку
     response = sender_stand_request.get_orders_track(track)
 
-# Проверяем, что код ответа = 200
+    # Проверяем, что код ответа = 200
     assert response.status_code == 200
